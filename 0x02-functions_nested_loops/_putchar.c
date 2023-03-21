70a0738828107c3eeb5ee8e076204dccd26a61ae
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-21 16:24:52 +0300

    update

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -4,6 +4,7 @@
  * int _putchar - writes the character c to stdout
  * main - Writes a program that prints _putchar, followed by a new line.
  * Return: 0 on success
+ * char c is the return value of main
  */
 int _putchar(char c)
 {
