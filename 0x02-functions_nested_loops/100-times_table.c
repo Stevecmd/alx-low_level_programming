fe6c052f8e0481963df3408bf46f742d01a89b06
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-21 18:25:00 +0300

    minor correction on 100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_times_table - prints the times table n times starting with 0
- * @s: number of times to print the table
+ * @n: number of times to print the table
  * written by Steve
  */
 void print_times_table(int n)
