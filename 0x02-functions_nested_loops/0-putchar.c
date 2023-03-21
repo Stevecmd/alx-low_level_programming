867007151db46029146abb471eb73b785ec2b058
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-21 17:09:26 +0300

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,14 @@
 #include "main.h"
 /**
- * int main(void) - short description
+ * main - prints _putchar
  * Description - writes a program that 
  * prints _putchar, followed by a new line
+ * written by Steve
  * Return ends the script
- * void is a return value of main
  */
 int main(void)
 {
 _putchar('_');
-	/**
-	 * @p: This is longer description of C
-	 *
-	 * Description: You can use paragraphs to describe arguments
-	 * using this method.
-	 */
 _putchar('p');
 _putchar('u');
 _putchar('t');
