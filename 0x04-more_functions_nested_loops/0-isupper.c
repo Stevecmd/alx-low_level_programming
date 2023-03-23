cfaa6f734a0de6232ac38c82c9a47d97dab418a1
Stevecmd <murimi101@gmail>
2023-03-23 04:53:13 -0700

    Restarted 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,13 +2,13 @@
 
 /**
  * _isupper - Check if a letter is in Uppercase
- * @x: The number to be checked
+ * @n: The number to be checked
  * Return: 1 if true and 0 for anything else
  */
 
 int _isupper(int n)
 {
-	if(n >= 65 && n<= 90)
+	if(n >= 'A' && n<= 'Z')
 	{
 	return(1);
 	}
