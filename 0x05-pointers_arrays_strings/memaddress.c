51f9dbb112d7081458a550cdb05e2242f3f7e6ae
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 21:54:18 +0300

    upload of pointers demo

--- 0x05-pointers_arrays_strings/memaddress.c
+++ 0x05-pointers_arrays_strings/memaddress.c
@@ -4,7 +4,10 @@ int main(void)
 {
     int i;
     char s;
+    int *p;
 
     printf("\nThe memory address of variable 'i': %p\n",&i);
     printf("The memory address of variable 's': %p\n",&s);
+    printf("The size of a pointer on this machine is: %lu\n", sizeof(p));
+    return (0);
 }
---
+++ 0x05-pointers_arrays_strings/pointers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int *p;
+    
+    n = 100;
+    p = %n;
+
+    printf("\nThe memory address of 'n' is: %p\n", &n);
+    printf("\nThe value of the pointer 'p' is: %p\n", p);
+    return (0);
+}
