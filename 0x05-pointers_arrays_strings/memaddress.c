6bc3d6a9ac9443406955eb9c51c9caa5b0abf1b1
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 21:38:57 +0300

    memaddress function

---
+++ 0x05-pointers_arrays_strings/memaddress.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    char s;
+
+    printf("\nThe memory address of variable 'i': %p\n",&i);
+    printf("The memory address of variable 's': %p\n",&s);
+}
