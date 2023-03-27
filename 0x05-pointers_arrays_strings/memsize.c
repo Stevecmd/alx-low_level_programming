2877ec68e8aa766f155297e89333620a3d15c3af
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 21:31:46 +0300

    added a line space

--- 0x05-pointers_arrays_strings/memsize.c
+++ 0x05-pointers_arrays_strings/memsize.c
@@ -7,6 +7,6 @@ int main(void)
     printf("The size of int on my machine is %lu\n", sizeof(int));
     printf("The size of char on my machine is %lu\n", sizeof(char));
     printf("The size of float on my machine is %lu\n", sizeof(float));
-    printf("The size of i on my machine is %lu", sizeof(i));
+    printf("The size of i on my machine is %lu\n", sizeof(i));
     return (0);
 }
