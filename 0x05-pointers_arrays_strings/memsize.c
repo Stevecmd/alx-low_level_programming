a27fdffac162e0c56d98e85f1c8b270f2f91b9bb
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 21:27:17 +0300

    Memory size function

---
+++ 0x05-pointers_arrays_strings/memsize.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    printf("The size of int on my machine is %lu\n", sizeof(int));
+    printf("The size of char on my machine is %lu\n", sizeof(char));
+    printf("The size of float on my machine is %lu\n", sizeof(float));
+    printf("The size of i on my machine is %lu", sizeof(i));
+    return (0);
+}
