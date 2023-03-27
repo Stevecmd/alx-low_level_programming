8f902c1d52070245e5516dab65a19ef2b97f4c9e
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 21:59:44 +0300

    print value of n

--- 0x05-pointers_arrays_strings/pointers.c
+++ 0x05-pointers_arrays_strings/pointers.c
@@ -8,6 +8,7 @@ int main(void)
     i = 100;
     p = &i;
 
+    printf("The value of 'n' is: %d\n", n);
     printf("\nThe memory address of 'n' is: %p\n", &i);
     printf("\nThe value of the pointer 'p' is: %p\n", p);
     return (0);
