8b9711ae07fbe11a65972b9f22d0b33948d5dcf4
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 22:18:36 +0300

    Getting the address of a pointer

--- 0x05-pointers_arrays_strings/pointers.c
+++ 0x05-pointers_arrays_strings/pointers.c
@@ -11,5 +11,8 @@ int main(void)
     printf("The value of 'i' is: %d\n", i);
     printf("\nThe memory address of 'n' is: %p\n", &i);
     printf("\nThe value of the pointer 'p' is: %p\n", p);
+    *p = 500;
+    printf("\nAfter Georeferencing, the value of the pointer 'p' is now: %d\n", i);
+    printf("\nAfter Georeferencing, the address of the pointer 'p' is now: %p\n", &p);
     return (0);
 }
