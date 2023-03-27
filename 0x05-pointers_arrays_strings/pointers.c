9a866b931fe3fc1cc053279ee772cccf5b451e00
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2023-03-27 22:40:30 +0300

    Spelling mistake correction

--- 0x05-pointers_arrays_strings/pointers.c
+++ 0x05-pointers_arrays_strings/pointers.c
@@ -12,7 +12,7 @@ int main(void)
     printf("\nThe memory address of 'n' is: %p\n", &i);
     printf("\nThe value of the pointer 'p' is: %p\n", p);
     *p = 500;
-    printf("\nAfter Georeferencing, the value of the pointer 'p' is now: %d\n", i);
-    printf("\nAfter Georeferencing, the address of the pointer 'p' is now: %p\n", &p);
+    printf("\nAfter Dereferencing, the value of the pointer 'p' is now: %d\n", i);
+    printf("\nAfter Dereferencing, the address of the pointer 'p' is now: %p\n", &p);
     return (0);
 }
