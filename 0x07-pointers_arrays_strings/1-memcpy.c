be3c5767e8b8111b35be8087f3c7d0b0a530d043
Stevecmd <murimi101@gmail>
2023-04-03 13:12:01 -0700

    Attempt 2 of 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
- * _memcpy - Entry point of value to be copied
+ * _memcpy - Entry point of value
  * @dest: destination to copy value to
  * @src: source of input
  * @n: number of bytes from memory area
- * Return: Pointer to dest
+ * Return: Pointer to destination
  */
 
- char *_memcpy(char *dest, char *src, unsigned int n)
- {
-    unsigned int a = 0;
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int a = 0;
+	int b = n;
 
-    for (a = 0; a < n; a++)
-        dest[a] = src[a];
-    return (dest);
- }
+	for (; a < b; a++)
+	{
+		dest[a] = src[a];
+		n--;
+	}
+	return (dest);
+}
