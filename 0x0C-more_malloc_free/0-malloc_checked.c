3ccc61f328aecbc84aa26e47185595c634c538bc
Stevecmd <murimi101@gmail>
2023-04-17 02:05:17 -0700

    0-malloc_checked.c and README

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,10 +10,10 @@
 		
 void *malloc_checked(unsigned int b)
 {
-	void *ptr = malloc(b);
+	void *pointer = malloc(b);
 
-	if (ptr == NULL)
-		exit(98);
+	if (pointer == NULL)
+			exit(98);
 
-	return (ptr);		
+	return (pointer);		
 }
