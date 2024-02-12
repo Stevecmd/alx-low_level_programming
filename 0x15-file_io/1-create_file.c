2d2352340f4d41b5d29f5764cae68f1876a3d207
Stevecmd <murimi101@gmail.com>
2024-02-12 07:52:14 -0800

    Fixed betty style on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,27 +11,27 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int file_desc, _write;
-    size_t text_len = (text_content != NULL) ? strlen(text_content) : 0;
+	int file_desc, _write;
+	size_t text_len = (text_content != NULL) ? strlen(text_content) : 0;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (file_desc == -1)
-        return (-1);
+	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file_desc == -1)
+		return (-1);
 
-    if (text_len > 0)
-    {
-        _write = write(file_desc, text_content, text_len);
-        if (_write == -1 || (size_t)_write != text_len)
-        {
-            close(file_desc);
-            return (-1);
-        }
-    }
+	if (text_len > 0)
+	{
+		_write = write(file_desc, text_content, text_len);
+		if (_write == -1 || (size_t)_write != text_len)
+	{
+		close(file_desc);
+		return (-1);
+	}
+	}
 
-    close(file_desc);
-    return (1);
+	close(file_desc);
+	return (1);
 }
 
