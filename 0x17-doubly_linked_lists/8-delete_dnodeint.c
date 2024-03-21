b787d3f9bd4ca7b5b68654c10e8e17334524a941
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-03-21 22:32:07 +0300

    8-delete_dnodeint.c update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,32 +9,32 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current_node = *head;
+	dlistint_t *current_node = *head;
 
-    if (*head == NULL)
-        return (-1);
+	if (*head == NULL)
+		return (-1);
 
-    for (; index != 0; index--)
-    {
-        if (current_node == NULL)
-            return (-1);
-        current_node = current_node->next;
-    }
+	for (; index != 0; index--)
+	{
+		if (current_node == NULL)
+			return (-1);
+		current_node = current_node->next;
+	}
 
-    if (current_node == *head)
-    {
-        *head = current_node->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-    }
-    else
-    {
+	if (current_node == *head)
+	{
+		*head = current_node->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+	}
+	else
+	{
 
-        current_node->prev->next = current_node->next;
-        if (current_node->next != NULL)
-            current_node->next->prev = current_node->prev;
-    }
+		current_node->prev->next = current_node->next;
+		if (current_node->next != NULL)
+			current_node->next->prev = current_node->prev;
+	}
 
-    free(current_node);
-    return (1);
+	free(current_node);
+	return (1);
 }
