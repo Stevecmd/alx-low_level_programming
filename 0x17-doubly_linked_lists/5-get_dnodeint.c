596368af54387ecb5c535bd349dc7438b5a77def
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-03-21 22:36:45 +0300

    update 4

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,17 +10,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i;
-    
-    if (head == NULL)
-        return (NULL);
-    
-    for (i = 0; i < index; i++)
-    {
-        if (head == NULL)
-            return (NULL);
-        head = head->next;
-    }
+	unsigned int i;
+	
+	if (head == NULL)
+		return (NULL);
+	
+	for (i = 0; i < index; i++)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+	}
 
-    return (head);
+	return (head);
 }
