5901eb2d935f8fb0f9d5ecf3b77f5989567871eb
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-03-21 22:34:48 +0300

    6-sum_dlistint.c update

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,8 +4,10 @@
  * sum_dlistint - Computes the sum of all data elements in a doubly linked list.
  * @head: A pointer to the head of the doubly linked list.
  *
- * This function iterates through a doubly linked list and calculates the sum of
- * all the integer data elements stored in each node. It returns the total sum
+ * This function iterates through a doubly linked list
+ * and calculates the sum of
+ * all the integer data elements stored in each node.
+ * It returns the total sum
  * of all data elements in the list.
  *
  * Return: The sum of all the data elements in the doubly linked list.
