a9eb4dde3aba58cb144a5b484d9c306d51cda5e9
Steve Murimi <72811315+Stevecmd@users.noreply.github.com>
2024-03-21 21:30:01 +0300

    test

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (head == NULL)
 		return (NULL);
 
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
+
 	if (new == NULL)
 		return (NULL);
 
