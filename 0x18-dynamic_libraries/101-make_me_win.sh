b668a9259b52b75ba20b55b2a41f89d1d8868f5e
Stevecmd <murimi101@gmail.com>
2024-03-25 17:06:23 +0300

    test 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 sleep 98
 wget -P .. https://raw.githubusercontent.com/Stevecmd/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD="$PWD/../libhack.so"
