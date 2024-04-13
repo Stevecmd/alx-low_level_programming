75e7a448ba4105be6c4c5a98be09f0ba72dcfdeb
Stevecmd <murimi101@gmail.com>
2024-04-13 12:15:44 +0300

    updated returns

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,7 +17,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     hash_node_t *new_node;
 
     if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-        return 0;
+        return (0);
 
     index = key_index((const unsigned char *)key, ht->size);
 
@@ -30,11 +30,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         {
             char *value_copy = strdup(value);
             if (!value_copy)
-                return 0;
+                return (0);
 
             free(current->value);
             current->value = value_copy;
-            return 1;
+            return (1);
         }
         current = current->next;
     }
@@ -42,26 +42,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     /* Key does not exist, create a new node and insert it */
     new_node = malloc(sizeof(hash_node_t));
     if (!new_node)
-        return 0;
+        return (0);
 
     new_node->key = strdup(key);
     if (!new_node->key)
     {
         free(new_node);
-        return 0;
+        return (0);
     }
 
     new_node->value = strdup(value);
     if (!new_node->value)
     {
         free(new_node->key);
-        free(new_node);
-        return 0;
+        free(new_node); /* Free the newly allocated node */
+        return (0);
     }
 
     /* Insert the new node at the beginning of the linked list */
     new_node->next = ht->array[index];
     ht->array[index] = new_node;
 
-    return 1;
+    return (1);
 }
