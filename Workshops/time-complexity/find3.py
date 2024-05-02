86dadd9aaed72bc72403521da7aec3df429ebb06
Stevecmd <murimi101@gmail.com>
2024-05-01 22:48:22 -0700

    New file

--- Workshops/time-complexity/find3.py
+++ Workshops/time-complexity/find3.py
@@ -14,6 +14,19 @@ def get_index_val(nums, index):
     val = nums[index]
     return
 
+def get_letters():
+    letters = "abc"
+    return letters
+
+def print_name(name):
+    to_print = f"{name}" # O(n)
+    print(name) # O(n)
+
+# what is the type of name?
+# name is a string
+# what is the maximum length of the string or name?
+# assuming len(name) = n and 1 <= n < 10 ** 7
+
 nums = [1,2,3,2,4,6,2,7]
 print(find(nums, 3))
 print(find(nums, 8))
